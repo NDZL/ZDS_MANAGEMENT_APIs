6e1d98e14653a12f8b8485dd185f110dd6109221
NDZL <cxnt48@gmail.com>
2021-09-21 18:29:32 +0200

    Unirest connectTimeout returns ZDS previous data

--- src/com/ndzl/ZDSapi.java
+++ src/com/ndzl/ZDSapi.java
@@ -1,14 +1,17 @@
 package com.ndzl;
 
+import kong.unirest.Unirest;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import org.json.*;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.*;
+import java.net.*;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ZDSapi {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
@@ -45,31 +48,62 @@ public class ZDSapi {
         request = new Request.Builder()
                 .url("https://api.zebra.com/v2/devices/readers/FX7500801791/os")
                 .method("GET", null)
-                .addHeader("apikey", Secrets.client_id)
-                .addHeader("Authorization", "Bearer "+zdsAuthToken)
+                .addHeader("Authorization", "Bearer " + zdsAuthToken)
                 .build();
         response = client.newCall(request).execute();
-        strResp=response.body().string();
-        System.out.println("AVAILABLE FW "+ strResp);
+        strResp = response.body().string();
+        response.close();
+        System.out.println("AVAILABLE FW " + strResp);
 
 
-        // 3.- CURRENT FW VERSION ON READER
-        request = new Request.Builder()
-                .url("https://api.zebra.com/v2/devices/readers/FX7500801791/version")
-                .method("GET", null)
-                .addHeader("apikey", Secrets.client_id)
-                .addHeader("Authorization", "Bearer "+zdsAuthToken)
-                .build();
+        //3b.- same as 3 with UNIREST library
+        //according to the postman doc, unirest is Unirest in Java: Simplified, lightweight HTTP client library.
+        //kong.github.io/unirest-java/
+
         try {
-            response = client.newCall(request).execute();
-        } catch (java.net.SocketTimeoutException stoe) {
-            System.out.println("TIMEOUT ERROR: "+response.header("Status-Code"));
+            Unirest.config()
+                    .connectTimeout(20000); //IMPORTANTE PER OTTENERE IL BODY ANCHE IN CASO DI TIMEOUT
+            kong.unirest.HttpResponse<String> responseunirest =
+                    Unirest
+                            .get("https://api.zebra.com/v2/devices/readers/FX7500801791/version")
+                            .header("accept", "application/json")
+                            .header("Authorization", "Bearer " + zdsAuthToken)
+                            .asString();
+            JSONObject jsonUNIREST_FW = new JSONObject(responseunirest.getBody());
+            if (responseunirest.getStatus() == 200)
+                System.out.println("UNIREST - FW READER APPLICATION  " + jsonUNIREST_FW.getString("readerApplication"));
+            else
+                System.out.println("UNIREST - ERROR READING: " + responseunirest.getBody());
+
         }
-        strResp=response.body().string();
-        JSONObject jsonFW = new JSONObject(strResp);
-        System.out.println("FW READER APPLICATION  "+ jsonFW.getJSONObject("value").getString("readerApplication"));
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+
+
     }
 
 
 
+}
+
+class ParameterStringBuilder {
+    public static String getParamsString(Map<String, String> params)
+            throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            result.append("&");
+        }
+
+        String resultString = result.toString();
+        return resultString.length() > 0
+                ? resultString.substring(0, resultString.length() - 1)
+                : resultString;
+    }
 }
